6b27ef76059c3254a61483bd64104b7b98969fc6
msantosifs <marcelo.rosa@academico.ifs.edu.br>
2024-03-04 10:30:39 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import {UsersComponent} from "./components/users/users.component";
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'users', component: UsersComponent },
+  { path: 'users-list', component: UsersComponent },
 ];
